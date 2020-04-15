3611b2e1b2bebe818ad1d914f73e5a28ae1863f5
WildSteph <Stephaine.Pantiez@gmail.com>
2020-04-15 16:29:05 +0200

    refactor(app): add import searchfighter in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import SearchFighter from './components/SearchFighter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header" />
+      <SearchFighter />
     </div>
 
   );
