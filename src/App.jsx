bb670fc462036392154644a9b04df39239d721a4
Ricefab02 <roger.fabrice02@gmail.com>
2020-04-15 14:26:48 +0200

    refactor: delete import FighterInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import MyFighterInput from './components/FighterInput'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <MyFighterInput />
-      </header>
+      <header className="App-header" />
     </div>
   );
 }
