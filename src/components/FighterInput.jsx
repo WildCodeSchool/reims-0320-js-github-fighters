6b1d7af98d94150099a0cf26813ca107ba696b14
Ricefab02 <roger.fabrice02@gmail.com>
2020-04-15 14:46:23 +0200

    refactor: change indetation and single qote after install ESLint

--- src/components/FighterInput.jsx
+++ src/components/FighterInput.jsx
@@ -1,26 +1,27 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class MyFighterInput extends Component {
   constructor(props) {
     super(props);
-    this.state = { fighter: "" };
+    this.state = { fighter: '' };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
-          this.setState({ fighter: event.target.value });
+    this.setState({ fighter: event.target.value });
   }
 
   render() {
     return (
       <div>
-        <label htmlFor="fighter">Fighter :</label>
+         <label htmlFor="fighter">Fighter :</label>
         <input
           id="fighter"
           type="text"
           value={this.state.fighter}
           onChange={this.handleChange}
         />
+        <h1>{this.state.fighter}</h1>
       </div>
     );
   }
