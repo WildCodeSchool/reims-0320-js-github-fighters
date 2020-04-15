17ccfea44520f8eb1a6a073867bf31489f90ca58
Ricefab02 <roger.fabrice02@gmail.com>
2020-04-15 16:49:20 +0200

    refactor: add Fighterinput in SearchFighter

--- src/components/SearchFighter.jsx
+++ src/components/SearchFighter.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import FighterInput from './FighterInput';
 
 class SearchFighter extends React.Component {
   constructor(props) {
@@ -9,7 +10,10 @@ class SearchFighter extends React.Component {
 
   render() {
     return (
-      <div />
+      <>
+        <FighterInput />
+        
+      </>
     );
   }
 }
