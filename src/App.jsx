e551f3481e8fc1e9debb9261b5abdf4bf7b4494a
WildSteph <Stephaine.Pantiez@gmail.com>
2020-04-15 14:18:53 +0200

    refactor(app): supertion des modification dans App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import SearchButton from './components/SearchButton'
 
 function App() {
   return (
     <>
-      <SearchButton />
     </>
   );
 }
