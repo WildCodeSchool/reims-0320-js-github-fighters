6838d30b3331c3cb2f977212e7d01b4d66b581e3
Ricefab02 <roger.fabrice02@gmail.com>
2020-04-15 11:33:54 +0200

    build: components/FighterInput.jsx

---
+++ src/components/FighterInput.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class MyFighterInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { fighter: "" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+          this.setState({ fighter: event.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <label htmlFor="fighter">Fighter :</label>
+        <input
+          id="fighter"
+          type="text"
+          value={this.state.fighter}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+}
+
+export default MyFighterInput;
