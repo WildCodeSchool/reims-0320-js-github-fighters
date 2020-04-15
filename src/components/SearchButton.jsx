d5816ae1eff089f7a0f53d6a3240be37f03580ef
WildSteph <Stephaine.Pantiez@gmail.com>
2020-04-15 14:20:38 +0200

    refactor(searchbutton): utilisation de metode ESLINT

--- src/components/SearchButton.jsx
+++ src/components/SearchButton.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 
 class SearchButton extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        searchbutton:"Rechercher"
-      };
-    }
-    render() {
-        return (
-          <div>
-            <button
-              onClick={event => {
-                console.log("hello");
-                }}
-             >{this.state.searchbutton}</button>
-                 </div>
-                 )
-                }
-            }
-export default SearchButton
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchbutton: 'Rechercher',
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <button
+          onClick={(event) => {
+            console.log('hello');
+          }}
+        >
+          {this.state.searchbutton}
+        </button>
+      </div>
+    );
+  }
+}
+
+export default SearchButton;
