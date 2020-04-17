344bed28314424b73067b34157a78728e2eda7a1
Ricefab02 <roger.fabrice02@gmail.com>
2020-04-17 09:54:22 +0200

    refactor(app): add import API with Axios and import FighterCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
 import React from 'react';
+import Axios from 'axios';
 import './App.css';
 import SearchFighter from './components/SearchFighter';
+import FighterCard from './components/FighterCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header" />
-      <SearchFighter />
-    </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      keywords: null,
+      infos: [],
+    };
+    this.search = this.search.bind(this);
+  }
 
-  );
+  setKeywords = (keywords) => this.setState({ keywords });
+
+  search = () => {
+    Axios.get(`https://api.github.com/users/${this.state.keywords}`)
+      .then((response) => response.data)
+      // Use this data to update the state
+      .then((data) => {
+        this.setState({
+          infos: data,
+        });
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <SearchFighter
+            setKeywords={this.setKeywords}
+            onSearch={this.search}
+          />
+
+          <FighterCard infos={this.state.infos} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
