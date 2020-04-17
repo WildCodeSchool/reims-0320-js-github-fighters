0a426ef1060d18c277e72b8ab274f7fecb533a73
brithe00 <bthellier51@gmail.com>
2020-04-17 09:38:31 +0200

    refactor(fightercard): added content in the divs where the api is called

--- src/components/FighterCard.jsx
+++ src/components/FighterCard.jsx
@@ -3,18 +3,32 @@ import React from 'react';
 function FighterCard({ infos }) {
   return (
     <div className="FighterCard">
-      <div className="FighterContainerImage" />
+      <div className="FighterContainerImage">
+        <div className="FighterImage">
+          <img src={infos.avatar_url} alt={infos.login} />
+        </div>
+      </div>
 
-      <div className="FighterContainerContent" />
+      <div className="FighterContainerContent">
+        <div className="FighterContainerName">
+          <div className="FighterName">{infos.name}</div>
+          <div className="FighterLogin">{infos.login}</div>
+        </div>
 
-      <div className="FighterBio" />
+        <div className="FighterBio">{infos.bio}</div>
 
-      <div className="FighterList" />
-
-
+        <div className="FighterList">
+          <ul className="FighterUnorderedList">
+            <li className="FighterCompany">{infos.company}</li>
+            <li className="FighterLocation">{infos.location}</li>
+            <li className="FighterBlog">
+              <a href="#">{infos.blog}</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-
-
   );
 }
+
 export default FighterCard;
