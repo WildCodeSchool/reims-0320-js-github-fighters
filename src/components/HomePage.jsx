277dd04dbe0ffcd7ac8046ae43a71e756742d3e9
WildSteph <Stephaine.Pantiez@gmail.com>
2020-04-27 10:21:36 +0200

    refactor(homepage.jsx): dealt rules in  HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -5,11 +5,6 @@ const HomePage = () => (
   <div>
     <h1 className="title">GitHub Fighter</h1>
     <h2 className="tagline">"become the next GitHub Champion!"</h2>
-    <div className="rules">
-      <p>win = 2 points</p>
-      <p>draw = 1 point</p>
-      <p>lost = 0 point</p>
-    </div>
   </div>
 );
 
