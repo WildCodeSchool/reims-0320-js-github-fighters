074d1934c12260d108eb292b41e2f24248bb1d85
WildSteph <Stephaine.Pantiez@gmail.com>
2020-04-27 09:58:25 +0200

    build(showwapons.jsx): add HomePage.jsx

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div>
+    <h1 className="title">GitHub Fighter</h1>
+    <h2 className="tagline">"become the next GitHub Champion!"</h2>
+    <div className="rules">
+      <p>win = 2 points</p>
+      <p>draw = 1 point</p>
+      <p>lost = 0 point</p>
+    </div>
+  </div>
+);
+
+export default HomePage;
