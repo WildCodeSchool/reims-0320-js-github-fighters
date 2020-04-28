4081392a5382f310139768a71dc9c58fa131c1e5
WildSteph <Stephaine.Pantiez@gmail.com>
2020-04-28 14:59:20 +0200

    refactor(homepage.jsx): add classname for css  in  HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import './HomePage.css';
 
 const HomePage = ({ start }) => (
-  <div>
+  <div className="homepage">
+    <img src="https://files.slack.com/files-pri/T6SG2QGG2-F011XH985QQ/fond-diapo-2.png" alt="fondgithub" />
     <h1 className="title">GitHub Fighter</h1>
-    <h2 className="tagline">"become the next GitHub Champion!"</h2>
+    <h2 className="tagline">" become the next GitHub Champion! "</h2>
     <button
+      className="buttonstart"
       onClick={start}
       type="button"
     >
-      start
+      START
     </button>
   </div>
 );
