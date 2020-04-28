d0e38b912cb6c0bdadb7faa8cf90018e282da9bf
WildSteph <Stephaine.Pantiez@gmail.com>
2020-04-28 15:00:32 +0200

    refactor(homepage.jsx): delete img in HomePage.Jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,7 +3,6 @@ import './HomePage.css';
 
 const HomePage = ({ start }) => (
   <div className="homepage">
-    <img src="https://files.slack.com/files-pri/T6SG2QGG2-F011XH985QQ/fond-diapo-2.png" alt="fondgithub" />
     <h1 className="title">GitHub Fighter</h1>
     <h2 className="tagline">" become the next GitHub Champion! "</h2>
     <button
