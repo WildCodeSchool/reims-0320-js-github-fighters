30950eaa6bc8298a9823d9e61f2f47671af173fa
brithe00 <bthellier51@gmail.com>
2020-05-12 18:03:03 +0200

    refactor(stepsjsx): changed class

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import './Steps.css'
+import './Steps.css';
 
 const Steps = () => (
   <div className="steps">
     <div className="steps-title">How to play</div>
 
     <ul className="steps-list">
-        <li><strong>Step : 1 </strong>Type a GitHub username in both fields and click 'Search'.</li>
-        <li><strong>Step : 2 </strong>Choose a weapon for your fighter and your challenger.</li>
-        <li><strong>Step : 3 </strong>Click on the 'START' button to start the fight, a random weapon is chosen to make it fair.</li>
+      <li className="steps-li">
+        <strong>Step : 1 </strong>
+        Type a GitHub username in both fields and click 'Search'.
+      </li>
+      <li className="steps-li">
+        <strong>Step : 2 </strong>
+        Choose a weapon for your fighter and your challenger.
+      </li>
+      <li className="steps-li">
+        <strong>Step : 3 </strong>
+        Click on the 'START' button to start the fight, a random weapon is chosen to make it fair.
+      </li>
     </ul>
 
   </div>
