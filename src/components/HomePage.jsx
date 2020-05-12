e2d54dc072a0124046d390284234dc587fb19357
WildSteph <Stephaine.Pantiez@gmail.com>
2020-05-12 14:24:53 +0200

    refactor(home): add Annimation in home

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import './HomePage.css';
+import { Animated } from 'react-animated-css';
 import InstructionPage from './InstructionPage';
 
 const HomePage = ({ start }) => (
-  <div className="homepage">
-    <h1 className="title">GitHub Fighters</h1>
-    <h2 className="tagline">" become the next GitHub Champion! "</h2>
-    <InstructionPage />
-    <button
-      className="buttonstart"
-      onClick={start}
-      type="button"
-    >
-      START
-    </button>
-  </div>
+
+  <Animated
+    animationIn="slideInUp"
+    animationOut="fadeOutUpBig"
+    animationInDuration={1400}
+    isVisible
+  >
+    <div className="homepage">
+      <h1 className="title">GitHub Fighters</h1>
+      <h2 className="tagline">" become the next GitHub Champion! "</h2>
+      <InstructionPage />
+      <button
+        className="buttonstart"
+        onClick={start}
+        type="button"
+      >
+        START
+      </button>
+    </div>
+  </Animated>
+
+
 );
 
 export default HomePage;
