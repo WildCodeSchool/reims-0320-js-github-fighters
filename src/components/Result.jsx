8d9b32072b7c9350ff8ebafcd86012729f30e392
Ricefab02 <roger.fabrice02@gmail.com>
2020-05-13 16:57:13 +0200

    build(result.jsx): created result

---
+++ src/components/Result.jsx
@@ -0,0 +1,11 @@
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
+
+const Result = () => {
+  const { state } = useContext(AppContext);
+  return (
+    <p>fighter score: {state.result.fighterScore} vs challenger score: {state.result.challengerScore}</p>
+  );
+}
+
+export default Result;
