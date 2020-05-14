1abecb396bfd4952f828e374a180dfd306a5d262
WildSteph <Stephaine.Pantiez@gmail.com>
2020-05-14 10:58:47 +0200

    refactor(result.jsx): add the winer is Fighter is favory

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -4,8 +4,20 @@ import AppContext from '../context/AppContext';
 const Result = () => {
   const { state } = useContext(AppContext);
   return (
-    <p>fighter score: {state.result.fighterScore} vs challenger score: {state.result.challengerScore}</p>
+    <>
+      {state.result.fighterScore >= state.result.challengerScore
+        ? <h1>Fighter WIN</h1>
+        : <h1>Challenger WIN</h1>}
+      <p>
+        fighter score:
+        {state.result.fighterScore}
+        {' '}
+        vs challenger score:
+        {state.result.challengerScore}
+      </p>
+
+    </>
   );
-}
+};
 
 export default Result;
