9ab1fe3098f3a3e7ff624b344547152b7778b246
Ricefab02 <roger.fabrice02@gmail.com>
2020-05-14 18:15:34 +0200

    refactor(footer.jsx): change copyright with link licence MIT

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,14 @@ const Footer = () => (
     <footer>
       <div className="footer-container">
         <div className="footer-logo-container">
-          GitHub Fighters
+          <div className="footer-title">GitHub Fighters</div>
+          <ul className="footer-list">
+            <li className="footer-li">
+              <a href="https://github.com/WildCodeSchool/reims-0320-js-github-fighters/blob/master/LICENCE.md"target="_blank" rel="noopener noreferrer">
+                © 2020 GitHub Fighters - All rights reserved.
+              </a>
+            </li>
+          </ul>
         </div>
         <div className="social-container">
           <div className="footer-title">Social</div>
@@ -16,18 +23,21 @@ const Footer = () => (
             <li className="footer-li">
               <a href="https://www.linkedin.com/in/roger-fabrice/" target="_blank" rel="noopener noreferrer">
                 Fabrice ROGER
+                {' '}
                 <FaLinkedin />
               </a>
             </li>
             <li className="footer-li">
               <a href="https://www.linkedin.com/in/stephaine-pantiez-a310b31a4/" target="_blank" rel="noopener noreferrer">
                 Stéphaine PANTIEZ
+                {' '}
                 <FaLinkedin />
               </a>
             </li>
             <li className="footer-li">
               <a href="https://www.linkedin.com/in/brian-thellier/" target="_blank" rel="noopener noreferrer">
                 Brian THELLIER
+                {' '}
                 <FaLinkedin />
               </a>
             </li>
@@ -40,6 +50,7 @@ const Footer = () => (
             <li className="footer-li">
               <a href="https://www.wildcodeschool.com/fr-FR/campus/reims" target="_blank" rel="noopener noreferrer">
                 Wild Code Shool
+                {' '}
                 <FaExternalLinkAlt />
               </a>
             </li>
@@ -52,15 +63,14 @@ const Footer = () => (
             <li className="footer-li">
               <a href="https://developer.github.com/v3/" target="_blank" rel="noopener noreferrer">
                 API
+                {' '}
                 <FaExternalLinkAlt />
               </a>
             </li>
           </ul>
         </div>
       </div>
-      <div className="rights-reserved">
-        © 2020 GitHub Fighters - All rights reserved.
-      </div>
+
     </footer>
   </>
 );
